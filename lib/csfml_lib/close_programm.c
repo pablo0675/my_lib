48b0586ed995840a7d753291206a99671916a983
Pablo Levy <pablo.levy@epitech.eu>
2022-04-19 16:11:27 +0200

    [ADD] personnal lib

---
+++ lib/csfml_lib/close_programm.c
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2022
+** lib
+** File description:
+** lib
+*/
+
+#include "my_rpg.h"
+
+static void close_window(game_t *game)
+{
+    my_printf("%s\n", "closing window");
+    sfRenderWindow_close(game->window->window);
+}
+
+static void close_sound(game_t *game)
+{
+    return;
+}
+
+static void close_music(game_t *game)
+{
+    my_printf("%s\n", "closing music");
+    sfMusic_destroy(game->music->game);
+    sfMusic_destroy(game->music->fights);
+    sfMusic_destroy(game->music->menu);
+    close_sound(game);
+}
+
+void close_cond(game_t *game)
+{
+    my_printf("%s%s\n", underlined_yellow, "closing programm...");
+    close_music(game);
+    destroy_all(game);
+    my_printf("%s", RESET);
+}
---
+++ lib/csfml_lib/csfml_includes.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2022
+** header
+** File description:
+** header
+*/
+
+#ifndef RPG_CSFML_INCLUDES_H
+    #define RPG_CSFML_INCLUDES_H
+    #include <SFML/Window.h>
+    #include <SFML/Graphics.h>
+    #include <SFML/Audio.h>
+
+#endif //RPG_CSFML_INCLUDES_H
